1cd1163ccc426b7a1505ca7f7d0d238f36336a2d
Mohammed Hamza <gh3173@wayne.edu>
2021-10-12 21:12:33 -0400

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import logo from '../resources/logo.svg';
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="Wrapper">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
